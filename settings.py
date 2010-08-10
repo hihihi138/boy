e5c8954096020b0f9185e918f81480a3be555802
Liu Jun <liujun@google.com>
2010-08-10 15:18:06 +0800

    put down the tag cloud.

--- settings.py
+++ settings.py
@@ -12,11 +12,11 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'bestyouku_mydata'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'bestyouku'             # Not used with sqlite3.
-DATABASE_PASSWORD = '3351808'         # Not used with sqlite3.
-DATABASE_HOST = 'mysql.alwaysdata.com'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = ROOT_PATH+'/bestyouku.db'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
